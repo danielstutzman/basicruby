053bac7f59329344d404944a114b263183a8c234
Daniel Stutzman <dtstutz@gmail.com>
2014-05-06 11:48:43 -0600

    Starting bytecode compiler can handle receiverless calls and int literals

--- lib/bytecode_compiler.rb
+++ lib/bytecode_compiler.rb
@@ -1,5 +1,49 @@
 class BytecodeCompiler
   def compile(ruby_code)
-    0
+    # expects Opal to already be required, so that the Opal compiler
+    # won't pull it all in when compiling this file
+    parser = Opal::Parser.new
+    sexp = parser.parse(ruby_code)
+    compile_expression sexp
+  end
+
+  private
+
+  class AssertionFailed < RuntimeError
+  end
+
+  def assert bool
+    raise AssertionFailed if !bool
+  end
+  def compile_expression sexp
+    head, tail = sexp[0], sexp[1..-1]
+    case head
+      when :call then compile_call tail
+      when :int  then compile_int tail
+      else no "unknown s-exp head #{head}"
+    end
+  end
+  def compile_call tail
+    bytecode = []
+    receiver, method, arglist = tail
+
+    assert arglist[0] == :arglist
+    arglist[1..-1].each do |arg|
+      bytecode.concat compile_expression(arg)
+      bytecode.push [:arg]
+    end
+
+    if receiver
+      bytecode.push compile_expression(receiver)
+    end
+
+    bytecode.push [:call, method]
+
+    bytecode
+  end
+  def compile_int tail
+    assert tail.size == 1
+    assert tail[0] == tail[0].to_i
+    [[:int, tail[0]]]
   end
 end
--- lib/bytecode_compiler_spec.rb
+++ lib/bytecode_compiler_spec.rb
@@ -1,8 +1,16 @@
+require 'opal'
 require './bytecode_compiler'
 
+Compiler = BytecodeCompiler.new
+def compile ruby_code
+  Compiler.compile ruby_code
+end
+
 describe BytecodeCompiler, '#compile' do
-  it 'can compile a blank string' do
-    compiler = BytecodeCompiler.new
-    compiler.compile('').should == 0
+  it 'compiles 3' do
+    compile('3').should == [[:int, 3]]
+  end
+  it 'compiles puts 3' do
+    compile('puts 3').should == [[:int, 3], [:arg], [:call, :puts]]
   end
 end
