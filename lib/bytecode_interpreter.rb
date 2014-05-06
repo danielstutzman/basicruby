4ae0deeb0b9cca34d0e1bc4775122140ea658574
Daniel Stutzman <dtstutz@gmail.com>
2014-05-06 17:34:50 -0600

    Handle user-caused NoMethodError

--- lib/bytecode_interpreter.rb
+++ lib/bytecode_interpreter.rb
@@ -2,6 +2,9 @@ class BytecodeInterpreter
   class ResultIsUnassigned
   end
 
+  class ProgramTerminated < RuntimeError
+  end
+
   attr_accessor :pos, :partial_calls, :output, :result
 
   def initialize main, hash
@@ -22,28 +25,33 @@ class BytecodeInterpreter
   def step
     raise "No more instructions" if @pos.nil?
 
-    bytecodes = @hash[@pos]
-    bytecodes.each do |bytecode|
-      head, arg0 = bytecode
-      case head
-        when :start_call
-          @partial_calls.push []
-        when :arg
-          @partial_calls.last.push result
-        when :int
-          @result = arg0
-        when :nil
-          @result = nil
-        when :call
-          do_call arg0
-        when :done
-          @pos = nil
-        when :goto
-          @result = ResultIsUnassigned
-          @pos = arg0
-        else
-          raise "Unknown bytecode head #{head}"
+    begin
+      bytecodes = @hash[@pos]
+      bytecodes.each do |bytecode|
+        head, arg0 = bytecode
+        case head
+          when :start_call
+            @partial_calls.push []
+          when :arg
+            @partial_calls.last.push result
+          when :int
+            @result = arg0
+          when :nil
+            @result = nil
+          when :call
+            do_call arg0
+          when :done
+            @pos = nil
+          when :goto
+            @result = ResultIsUnassigned
+            @pos = arg0
+          else
+            raise "Unknown bytecode head #{head}"
+        end
       end
+    rescue ProgramTerminated => e
+      @output += "#{e.message}\n"
+      @pos = nil
     end
   end
 
@@ -67,15 +75,19 @@ class BytecodeInterpreter
         @result = args
       end
     else
-      if receiver == ResultIsUnassigned
-        receiver = @main
-      end
-      if receiver.private_methods.include?(method_name)
-        message = "private method `#{method_name}' called for " +
-          "#{receiver.inspect}:#{receiver.class}"
-        raise NoMethodError.new(message)
-      else
-        @result = receiver.send method_name, *args
+      begin
+        if receiver == ResultIsUnassigned
+          receiver = @main
+        end
+        if receiver.private_methods.include?(method_name)
+          message = "private method `#{method_name}' called for " +
+            "#{receiver.inspect}:#{receiver.class}"
+          raise NoMethodError.new(message)
+        else
+          @result = receiver.send method_name, *args
+        end
+      rescue NoMethodError => e
+        raise ProgramTerminated.new e
       end
     end
   end
