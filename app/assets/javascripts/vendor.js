e72a99bf11d8d8e6f7b8e58e816638087b40bde4
Daniel Stutzman <dtstutz@gmail.com>
2014-05-06 20:33:59 -0600

    Suppress React DevTools reminder

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -1,1 +1,9 @@
+//= require_self
 //= require_tree ../../../vendor/assets/javascripts/.
+
+var oldDebug = window.console.debug;
+window.console.debug = function() {
+  if (arguments[0] === 'Download the React DevTools for a better development experience: http://fb.me/react-devtools') { return; }
+  oldDebug.apply(window.console, arguments);
+};
+
