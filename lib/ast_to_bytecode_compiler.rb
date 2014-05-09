11e8b06de286354490605a7750723d547c2d8296
Daniel Stutzman <dtstutz@gmail.com>
2014-05-09 11:55:04 -0600

    Yet another implementation of AST-to-bytecode compiler

---
+++ lib/ast_to_bytecode_compiler.rb
@@ -0,0 +1,145 @@
+class AstToBytecodeCompiler
+  def initialize main
+    @main = main
+  end
+
+  def compile sexp
+    return [[:result, nil]] if sexp.nil?
+    case sexp[0]
+      when :int     then [[:token] + sexp.source, [:result, sexp[1]]]
+      when :float   then [[:token] + sexp.source, [:result, sexp[1]]]
+      when :str     then [[:token] + sexp.source, [:result, sexp[1]]]
+      when :nil     then [[:token] + sexp.source, [:result, nil]]
+      when :true    then [[:token] + sexp.source, [:result, true]]
+      when :false   then [[:token] + sexp.source, [:result, false]]
+      when :block   then compile_block sexp
+      when :call    then compile_call sexp
+      when :arglist then compile_arglist sexp
+      when :paren   then compile sexp[1]
+      when :lasgn   then compile_lasgn sexp
+      when :lvar    then compile_lvar sexp
+      when :if      then compile_if sexp
+      when :dstr    then compile_dstr sexp
+      when :evstr   then compile sexp[1]
+      else no "s-exp with head #{sexp[0]}"
+    end
+  end
+
+  private
+
+  class AssertionFailed < RuntimeError
+  end
+
+  class DebuggerDoesntYetSupport < RuntimeError
+  end
+
+  def assert bool
+    raise AssertionFailed if !bool
+  end
+
+  def no feature
+    raise DebuggerDoesntYetSupport.new(feature)
+  end
+
+  def compile_block sexp
+    _, *statements = sexp
+    bytecodes = []
+
+    statements.each_with_index do |statement, i|
+      if sexp.source
+        bytecodes.push [:position] + sexp.source
+      end
+      bytecodes.concat compile(statement)
+      if i < statements.size - 1
+        bytecodes.push [:discard]
+      end
+    end
+
+    bytecodes
+  end
+
+  def compile_call sexp
+    _, receiver, method_name, arglist = sexp
+    bytecodes = []
+    bytecodes.push [:start_call]
+
+    if receiver
+      bytecodes.concat compile(receiver)
+    else
+      bytecodes.push [:result, @main]
+    end
+    bytecodes.push [:arg]
+
+    bytecodes.push [:token] + sexp.source
+    bytecodes.push [:result, method_name]
+    bytecodes.push [:arg]
+    
+    bytecodes.concat compile(arglist)
+
+    bytecodes.push [:call]
+    bytecodes
+  end
+
+  def compile_arglist sexp
+    _, *args = sexp
+    bytecodes = []
+
+    args.each do |arg|
+      bytecodes.concat compile(arg)
+      bytecodes.push [:arg]
+    end
+
+    bytecodes
+  end
+
+  def compile_lasgn sexp
+    _, var_name, expression = sexp
+    compile(expression) + [[:token] + sexp.source, [:to_var, var_name]]
+  end
+
+  def compile_lvar sexp
+    _, var_name = sexp
+    [[:token] + sexp.source, [:from_var, var_name]]
+  end
+
+  def compile_if sexp
+    _, condition, then_block, else_block = sexp
+    bytecodes = []
+    bytecodes.concat compile(condition)
+    bytecodes.push [:push_if]
+    label_else = "else_#{sexp.source.join('_')}"
+    label_endif = "endif_#{sexp.source.join('_')}"
+    bytecodes.push [:if_goto, false, label_else]
+    if then_block && then_block.source
+      bytecodes.push [:position] + then_block.source
+    end
+    bytecodes.concat compile(then_block)
+    bytecodes.push [:if_goto, true, label_endif]
+    bytecodes.push [:label, label_else]
+    if else_block && else_block.source
+      bytecodes.push [:position] + else_block.source
+    end
+    bytecodes.concat compile(else_block)
+    bytecodes.push [:label, label_endif]
+    bytecodes.push [:pop_if]
+    bytecodes
+  end
+
+  def compile_dstr sexp
+    _, str, *strs_or_evstrs = sexp
+    bytecodes = []
+    bytecodes.push [:start_call]
+    bytecodes.push [:result, @main]
+    bytecodes.push [:arg]
+    bytecodes.push [:result, :__STR_INTERP]
+    bytecodes.push [:arg]
+    bytecodes.push [:result, str]
+    bytecodes.push [:arg]
+    strs_or_evstrs.each do |str_or_evstr|
+      bytecodes.concat compile(str_or_evstr)
+      bytecodes.push [:arg]
+    end
+    bytecodes.push [:call]
+    bytecodes
+  end
+end
---
+++ lib/ast_to_bytecode_compiler_spec.rb
@@ -0,0 +1,75 @@
+require 'opal'
+require './ast_to_bytecode_compiler'
+
+$main = self
+
+def compile ruby_code
+  parser = Opal::Parser.new
+  sexp = parser.parse ruby_code
+  compiler = AstToBytecodeCompiler.new $main
+  compiler.compile sexp
+end
+
+describe AstToBytecodeCompiler, '#compile' do
+  it 'compiles blank' do
+    compile('').should == [[:result, nil]]
+  end
+  it 'compiles 3' do
+    compile('3').should == [[:token, 1, 0], [:result, 3]]
+  end
+  it 'compiles puts 3' do
+    compile('puts 3').should == [
+      [:start_call],
+      [:result, $main], [:arg],
+      [:token, 1, 0], [:result, :puts], [:arg],
+      [:token, 1, 5], [:result, 3], [:arg],
+      [:call],
+    ]
+  end
+  it 'compiles 3; 4' do
+    compile('3; 4').should == [
+      [:token, 1, 0], [:result, 3], [:discard],
+      [:token, 1, 3], [:result, 4],
+    ]
+  end
+  it 'compiles (3)' do
+    compile('(3)').should == [[:token, 1, 1], [:result, 3]]
+  end
+  it 'compiles x = 3' do
+    compile('x = 3').should == [
+      [:token, 1, 4], [:result, 3],
+      [:token, 1, 0], [:to_var, :x],
+    ]
+  end
+  it 'compiles x = 3; x' do
+    compile('x = 3; x').should == [
+      [:token, 1, 4], [:result, 3],
+      [:token, 1, 0], [:to_var, :x], [:discard],
+      [:token, 1, 7], [:from_var, :x],
+    ]
+  end
+  it 'compiles x = if true then 3 end' do
+    compile('x = if true then 3 end').should == [
+      [:token, 1, 7], [:result, true], [:push_if],
+      [:if_goto, false, "else_1_4"],
+      [:position, 1, 17],
+      [:token, 1, 17], [:result, 3],
+      [:if_goto, true, "endif_1_4"],
+      [:label, "else_1_4"],
+      [:result, nil],
+      [:label, "endif_1_4"],
+      [:pop_if],
+      [:token, 1, 0], [:to_var, :x],
+    ]
+  end
+  it 'compiles "1#{2}3"' do
+    compile('"1#{2}3"').should == [
+      [:start_call], [:result, $main], [:arg],
+      [:result, :__STR_INTERP], [:arg],
+      [:result, "1"], [:arg],
+      [:token, 1, 4], [:result, 2], [:arg],
+      [:token, 1, 6], [:result, "3"], [:arg],
+      [:call],
+    ]
+  end
+end
