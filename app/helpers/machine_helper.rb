ddead448df4e554dab5cae8bba11334f9c3b2e8e
Daniel Stutzman <dtstutz@gmail.com>
2014-05-16 16:52:14 -0600

    Convert backticks in exercise titles to <code> in breadcrumbs, and nothing in document <title>

--- app/helpers/machine_helper.rb
+++ app/helpers/machine_helper.rb
@@ -11,4 +11,10 @@ module MachineHelper
     lines = @exercise.yaml_loaded['assignment'].split("\n")
     lines.map { |line| h(line) }.join("<br>\n").html_safe
   end
+  def convert_title_backticks title
+    h(title).gsub(/`(.+)`/, '<code>\1</code>').html_safe
+  end
+  def document_title exercise
+    "#{exercise.path} #{exercise.title.gsub('`', '')}"
+  end
 end
