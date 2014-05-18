f0bc723988b21f2384c2418401d73dd7ba04c628
Daniel Stutzman <dtstutz@gmail.com>
2014-05-18 17:06:33 -0600

    New home page has grid of lessons x exercise types.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def image_tag_for_exercise exercise
+    image_tag_for_exercise_color exercise.color
+  end
+  def image_tag_for_exercise_color color
+    case color
+    when 'gold'
+      image_tag 'exercise_icons/light_bulb30.png', width: 60, height: 60
+    when 'red'
+      image_tag 'exercise_icons/red_bug30.png', width: 60, height: 60
+    when 'blue'
+      image_tag 'exercise_icons/question_mark30.png', width: 60, height: 60
+    when 'green'
+      image_tag 'exercise_icons/pen-and-paper-60.png', width: 60, height: 60
+    when 'orange'
+      image_tag 'exercise_icons/jack-o-lantern-60.png', width: 60, height: 60
+    end
+  end
 end
--- app/helpers/machine_helper.rb
+++ app/helpers/machine_helper.rb
@@ -1,14 +1,4 @@
 module MachineHelper
-  def image_tag_for_exercise exercise
-    case exercise.color
-    when 'gold'
-      image_tag 'exercise_icons/light_bulb30.png', width: 30, height: 30
-    when 'red'
-      image_tag 'exercise_icons/red_bug30.png', width: 30, height: 30
-    when 'blue'
-      image_tag 'exercise_icons/question_mark30.png', width: 30, height: 30
-    end
-  end
   def assignment_split_with_br exercise
     lines = @exercise.yaml_loaded['assignment'].split("\n")
     lines.map { |line| h(line) }.join("<br>\n").html_safe
