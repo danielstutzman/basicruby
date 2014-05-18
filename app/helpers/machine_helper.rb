c3c821068aa083dce76f065590336aa9a09c4793
Daniel Stutzman <dtstutz@gmail.com>
2014-05-18 17:07:02 -0600

    Added level-picker to top-right and skip/continue button to bottom-right.

--- app/helpers/machine_helper.rb
+++ app/helpers/machine_helper.rb
@@ -9,4 +9,13 @@ module MachineHelper
   def document_title exercise
     "#{exercise.path} #{exercise.title.gsub('`', '')}"
   end
+  def level_picker current_level_num
+    nbsp = "\xc2\xa0"
+    options = [5, 4, 3, 2, 1].map do |level_num|
+      "#{nbsp}Level #{level_num}#{nbsp}"
+    end
+    current_option = "#{nbsp}Level #{current_level_num}#{nbsp}"
+    select_tag :level, options_for_select(options, current_option),
+      class: 'level-picker'
+  end
 end
