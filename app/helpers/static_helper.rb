fbc035d1a157520d4d0cd31a343bfb5611c3018b
Daniel Stutzman <dtstutz@gmail.com>
2014-05-19 15:22:46 -0600

    Use icons for characters instead of unicode symbols, for precise positioning.  Also put blue column before red.

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,13 +1,5 @@
 module StaticHelper
   def level_indicator level_num
-    icon = case level_num
-      when 0 then '&#x25cb;'
-      when 1 then '&#x25d4;'
-      when 2 then '&#x25d1;'
-      when 3 then '&#x25d5;'
-      when 4 then '&#x25cf;'
-    end
-    html = "<a class='level-indicator level-#{level_num}' href='#'>#{icon}</a>"
-    html.html_safe
+    "<a class='level-indicator level-#{level_num}' href='#'></a>".html_safe
   end
 end
