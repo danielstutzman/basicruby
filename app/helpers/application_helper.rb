91c285997e5854fb804d70f0597adf48d86608f1
Daniel Stutzman <dtstutz@gmail.com>
2014-05-24 18:02:41 -0600

    Rescaled the yellow, blue, and red icons for 60x60 instead of 30x30

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,13 @@ module ApplicationHelper
   def image_tag_for_exercise_color color, size
     case color
     when 'yellow'
-      image_tag 'exercise_icons/light_bulb30.png',
+      image_tag 'exercise_icons/light_bulb_60.png',
         width: size, height: size
     when 'red'
-      image_tag 'exercise_icons/red_bug30.png',
+      image_tag 'exercise_icons/red_bug_60.png',
         width: size, height: size
     when 'blue'
-      image_tag 'exercise_icons/question_mark30.png',
+      image_tag 'exercise_icons/question_mark_60.png',
         width: size, height: size
     when 'green'
       image_tag 'exercise_icons/pen-and-paper-60.png',
