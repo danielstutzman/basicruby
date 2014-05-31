2663f4b320f84bb1f7f91ca9bd32eaa69161831a
Daniel Stutzman <dtstutz@gmail.com>
2014-05-30 20:27:28 -0600

    Split curriculum.yaml into one file per topic.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,22 @@ require 'yaml'
 require 'json'
 
 Exercise.transaction do
-  topics = YAML.load_file("#{File.dirname(__FILE__)}/curriculum.yaml")
-
   Topic.delete_all
   Exercise.delete_all
 
-  topics.each_with_index do |topic_yaml, num0|
-    topic = Topic.create! num: num0 + 1,
-      title: topic_yaml['title'],
-      title_html: topic_yaml['title_html'] || topic_yaml['title'],
-      features: topic_yaml['features']
+  dir = File.dirname(__FILE__)
+  topic_num = 1
+  Dir.glob("#{dir}/*.yaml") do |path|
+    yaml = YAML.load_file(path)
+    p yaml
+
+    topic = Topic.create! num: topic_num + 1,
+      title: yaml['title'],
+      title_html: yaml['title_html'] || yaml['title'],
+      features: yaml['features']
 
     %w[purple yellow blue red green orange].each do |color|
-      exercises = topic_yaml[color]
+      exercises = yaml[color]
       next if exercises.nil? # just for unfinished topics
       exercises.each_with_index do |exercise, rep_num0|
         Exercise.create! topic_id: topic.id, topic_num: topic.num,
@@ -22,5 +25,7 @@ Exercise.transaction do
           json: JSON.generate(exercise)
       end
     end
+
+    topic_num += 1
   end
 end
