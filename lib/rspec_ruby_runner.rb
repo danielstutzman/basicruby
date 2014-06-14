526c361aea184376cd6849d9344ca8b88d62149f
Daniel Stutzman <dtstutz@gmail.com>
2014-06-13 22:00:49 -0600

    Log bytecodes to text files.

--- lib/rspec_ruby_runner.rb
+++ lib/rspec_ruby_runner.rb
@@ -2,6 +2,8 @@ require './bytecode_interpreter.rb'
 require './bytecode_spool.rb'
 require 'opal'
 
+LOG = false
+
 class RspecRubyRunner
   def output_from ruby_code
     parser = Opal::Parser.new
@@ -12,15 +14,30 @@ class RspecRubyRunner
     sexp2 = parser.parse ruby_code
     bytecodes2 = compiler.compile_program 'user', sexp2
     spool = BytecodeSpool.new bytecodes1 + [[:discard]] + bytecodes2
+
+    if LOG
+      File.open 'bytecodes.txt', 'w' do |file|
+        bytecodes1.each { |bytecode| file.write bytecode.join(' ') + "\n" }
+        file.write "\n"
+        bytecodes2.each { |bytecode| file.write bytecode.join(' ') + "\n" }
+      end
+    end
+
     spool.queue_run_until 'DONE'
     interpreter = BytecodeInterpreter.new
     begin
-      while true
+      File.open 'trace.txt', 'w' do |file|
+        while true
         bytecode = spool.get_next_bytecode interpreter.is_result_truthy?,
-        break if bytecode.nil?
-        interpreter.interpret bytecode
-          interpreter.gosubbing_label, interpreter.gotoing_label,
-          interpreter.stack_size
+            interpreter.gosubbing_label, interpreter.gotoing_label,
+            interpreter.stack_size
+          break if bytecode.nil?
+          if LOG
+            file.write bytecode.join(' ') + "\n"
+          end
+
+          interpreter.interpret bytecode
+        end
       end
       interpreter.visible_state[:output].map { |pair| pair[1] }.join
     rescue ProgramTerminated => e
