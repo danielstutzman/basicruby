9c17e4ca12256756ee2cda9a1477504ec8db8d09
Daniel Stutzman <dtstutz@gmail.com>
2014-06-21 10:14:22 -0600

    Remove logging because it's been replaced with debug.rb.

--- lib/rspec_ruby_runner.rb
+++ lib/rspec_ruby_runner.rb
@@ -2,8 +2,6 @@ require './bytecode_interpreter.rb'
 require './bytecode_spool.rb'
 require 'opal'
 
-LOG = false
-
 class RspecRubyRunner
   def initialize
     parser = Opal::Parser.new
@@ -18,28 +16,15 @@ class RspecRubyRunner
     bytecodes2 = compiler.compile_program 'TestCode', sexp2
     spool = BytecodeSpool.new @bytecodes1 + [[:discard]] + bytecodes2
 
-    if LOG
-      File.open 'bytecodes.txt', 'w' do |file|
-        bytecodes1.each { |bytecode| file.write bytecode.join(' ') + "\n" }
-        file.write "\n"
-        bytecodes2.each { |bytecode| file.write bytecode.join(' ') + "\n" }
-      end
-    end
-
     spool.queue_run_until 'DONE'
     interpreter = BytecodeInterpreter.new
     begin
-      File.open 'trace.txt', 'w' do |file|
-        while true
-          bytecode = spool.get_next_bytecode
-          break if bytecode.nil?
-          if LOG
-            file.write bytecode.join(' ') + "\n"
-          end
+      while true
+        bytecode = spool.get_next_bytecode
+        break if bytecode.nil?
 
-          spool_command = interpreter.interpret bytecode
-          spool.do_command *spool_command if spool_command
-        end
+        spool_command = interpreter.interpret bytecode
+        spool.do_command *spool_command if spool_command
       end
       interpreter.visible_state[:output].map { |pair| pair[1] }.join
     rescue ProgramTerminated => e
