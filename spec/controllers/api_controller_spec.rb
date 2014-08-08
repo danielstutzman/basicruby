3dd04836ba1d1a2b144c7128d133bb5210d42de3
Daniel Stutzman <dtstutz@gmail.com>
2014-08-08 12:15:33 -0600

    Start controller spec.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ApiController do
-
+  describe 'GET #menu' do
+    it "returns the information for one test_model" do
+      learner = create(:learner)
+      get :menu, {format: :json}, {learner_id: learner.id}
+      JSON.parse(response.body) == { 'learner_id'=>1, 'topics'=>[] }
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,4 +45,6 @@ RSpec.configure do |config|
   end
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.render_views = true # needed to examine json in response
 end
