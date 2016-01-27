6242dcdc9cb10bab09e7f8d1057628870700e855
Dan Stutzman <dan@danstutzman.com>
2016-01-27 13:44:20 -0700

    Need some 'bundle config' commands to get bundle installing on Mac

---
+++ bundle_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+#echo "TODO: Make sure Postgres.app isn't in PATH"
+#brew install postgresql
+#sudo env ARCHFLAGS="-arch x86_64" gem install pg -- --with-pg-config=/usr/local/Cellar/postgresql/9.4.5_2/bin/pg_config
+bundle config build.eventmachine --with-cppflags=-I/usr/local/opt/openssl/include
+bundle config build.pg --with-pg-config=/usr/local/Cellar/postgresql/9.4.5_2/bin/pg_config
+ARCHFLAGS="-arch x86_64" bundle install --path vendor/bundle
