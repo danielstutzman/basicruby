28d7d2306f020f3b5873c1595eb971203d9a3800
Dan Stutzman <dan@danstutzman.com>
2016-05-16 19:49:20 -0600

    Bug fix: change from "EOF" to EOF broke the DROP TABLE commands.

--- cloud/pg_restore_to_cloud.sh
+++ cloud/pg_restore_to_cloud.sh
@@ -12,7 +12,8 @@ echo INSTANCE_IP=$INSTANCE_IP
 scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null $DUMPFILE root@$INSTANCE_IP:/tmp/$DUMPFILE
 
 ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@$INSTANCE_IP <<EOF
+set -e
 cd /tmp # eliminate warning
-for TABLE_NAME in `sudo -u postgres psql -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"`; do sudo -u postgres psql -c "drop table \$TABLE_NAME cascade"; done
+for TABLE_NAME in \`sudo -u postgres psql -t -c "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"\`; do sudo -u postgres psql -c "drop table \$TABLE_NAME cascade"; done
 cat /tmp/$DUMPFILE | sudo -u postgres psql -U postgres -d postgres
 EOF
